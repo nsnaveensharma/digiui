60e196911ee683f6806c1614aebf553fedbdf144
money <naveensharma.ns@outlook.com>
2022-02-14 21:41:31 +0530

    git push origin master

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DigiUi.Com | Best Web Application & Designing Company | Customized User Interface | Industrial Automation | Excel Automation | Custom ERP Software | Online Billing System</title>
+</head>
+<body>
+    
+</body>
+</html>
+
 <?php
 
 print('Hi');

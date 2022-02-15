e51b3d5d7a2410a46d933c72f5e38792c3e29887
Naveen Sharma <naveensharma.ns@outlook.com>
2022-02-15 10:51:55 +0530

    git commit -m 'done'

--- files/navbar.php
+++ files/navbar.php
@@ -1,5 +1,5 @@
 
-                <div class="flex flex-no-wrap">
+
                     <!-- Sidebar starts -->
                     <!-- Remove class [ hidden ] and replace [ sm:flex ] with [ flex ] -->
                     <div style="min-height: 716px" class="w-64 absolute sm:relative bg-gray-800 shadow md:h-full flex-col justify-between hidden sm:flex">
@@ -242,10 +242,5 @@
                     </div>
                     <!-- Sidebar ends -->
                     <!-- Remove class [ h-64 ] when adding a card block -->
-                    <div class="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
-                        <!-- Remove class [ border-dashed border-2 border-gray-300 ] to remove dotted border -->
-                        <div class="w-full h-full rounded border-dashed border-2 border-gray-300">
-                            <!-- Place your content here -->
-                        </div>
-                    </div>
-                </div>
+
+                
--- index.php
+++ index.php
@@ -8,8 +8,15 @@
 
 </head>
 <body>
+<div class="flex flex-no-wrap">
 <?php include('files/navbar.php'); ?>
-
+<div class="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
+                        <!-- Remove class [ border-dashed border-2 border-gray-300 ] to remove dotted border -->
+                        <div class="w-full h-full rounded border-dashed border-2 border-gray-300">
+                            <!-- Place your content here -->
+                        </div>
+                    </div>
+</div>
 </body>
 <script src="https://cdn.tailwindcss.com"></script>
 <script src="src/js/main.js"></script>
